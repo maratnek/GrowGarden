5472f2f3f10d5ad788f704be87dba1865de5b6bd
zm <maratn566ek@gmail.com>
2017-04-06 00:20:16 +0300

    angular markup

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'Рассада помидор';
+}
