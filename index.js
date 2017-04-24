6085d564309fec9ef0fcf0c71511ac3eb42438b0
maratnek <maratn566ek@gmail.com>
2017-04-24 17:05:18 +0300

    message

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ app.set('public', __dirname + '/public');
 //app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('index');
+	res.send('Invalid Endpoint');
+  // response.render('index');
 });
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
