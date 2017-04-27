c3b898c09d44e7308b222eede59ff0f2859a64c0
maratnek <maratn566ek@gmail.com>
2017-04-27 19:23:14 +0300

    optimize project

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,10 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser }    from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { AppModule } from './app/app.module';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+import { AppModuleNgFactory } from './aot/app/app.module.ngfactory';
+console.log('Running AOT compiled');
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
